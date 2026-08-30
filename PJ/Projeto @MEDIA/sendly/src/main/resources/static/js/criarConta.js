e1732857471c8b0fa5b214d0c3147e1d39faf735
Matheuszin <matheus.com.0512@gmail.com>
2026-08-30 13:47:49 -0300

    CadastroNew 1.0.4v

--- PJ/Projeto @MEDIA/sendly/src/main/resources/static/js/criarConta.js
+++ PJ/Projeto @MEDIA/sendly/src/main/resources/static/js/criarConta.js
@@ -5,21 +5,10 @@ const header = document.querySelector('header');
 
 form.addEventListener('submit', async (e) => {
     
-    e.preventDefault();
-
+  
     const dados = new FormData(form);
 
-    let data = await fetch('https://pwjob-production-1606.up.railway.app/cadastro',{method: 'POST', body: dados});
+    let data = await fetch('https://pwjob-production-1606.up.railway.app/cadastroNew',{method: 'POST', body: dados});
     data = await data.json();
-    
-    if(data.id){
-        sessionStorage.setItem('usuarioId', data.id);
-        window.location.href = './menuprincipal.html'
-    }else{
-        p.forEach(noLogin => {
-            noLogin.innerHTML = data.mensagem;
-            noLogin.style.color = "red";
-        });
-    };
 });
 
